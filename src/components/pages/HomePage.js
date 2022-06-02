1f9364e640ddca7c6c6a3ad095995a9d1ec64653
sauravrajleaf <saurved785@gmail.com>
2022-06-03 02:44:05 +0530

    fixed error in useEffect api call

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -6,25 +6,25 @@ import { useEffect, useState } from "react";
 import { Pizzas } from "../pizzas/pizzalist/Pizzas";
 
 export const HomePage = () => {
-  const [pizzaDetails, setData] = useState([]);
+	const [pizzaDetails, setData] = useState([]);
 
-  useEffect(() => {
-    const fetchPost = async () => {
-      const response = await axios.get(
-        "https://run.mocky.io/v3/ec196a02-aaf4-4c91-8f54-21e72f241b68"
-      );
-      // console.log(response.data);
+	useEffect(() => {
+		fetchPost();
+	}, []);
+	const fetchPost = async () => {
+		const response = await axios.get(
+			"https://run.mocky.io/v3/ec196a02-aaf4-4c91-8f54-21e72f241b68"
+		);
+		// console.log(response.data);
 
-      const apiData = response.data;
+		const apiData = response.data;
 
-      setData(apiData);
-      // console.log(pizzaDetails);
-    };
-    fetchPost();
-  }, [pizzaDetails]);
-  return (
-    <div>
-      <Pizzas pizzaDetails={pizzaDetails} />
-    </div>
-  );
+		setData(apiData);
+		// console.log(pizzaDetails);
+	};
+	return (
+		<div>
+			<Pizzas pizzaDetails={pizzaDetails} />
+		</div>
+	);
 };
